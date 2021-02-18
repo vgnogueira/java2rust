b5ba5882f58055fe3a8c9c7115358261f1680e62
vgnogueira <valter@fastway.com.br>
2021-02-18 00:26:40 -0300

    housekeeping

--- antlr4-java-parser/run.sh
+++ antlr4-java-parser/run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-export CLASSPATH=".:/usr/local/lib/antlr-4.8-complete.jar:$CLASSPATH"
-alias antlr4='java -jar /usr/local/lib/antlr-4.8-complete.jar'
+export CLASSPATH=".:./antlr-4.8-complete.jar:$CLASSPATH"
+alias antlr4='java -jar ./antlr-4.8-complete.jar'
 alias grun='java org.antlr.v4.gui.TestRig'
 
 antlr4 Java9.g4 -o parser
 
 cd parser
 
+export CLASSPATH=".:../antlr-4.8-complete.jar:$CLASSPATH"
 javac *.java
 
 grun Java9 compilationUnit *.java
